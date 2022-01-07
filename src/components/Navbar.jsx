828b9eae9940d2aa5e0995873c7f5aa91a489380
brain <shahriarmehedi95@gmail.com>
2022-01-08 00:04:27 +0600

    material ui

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar, Typography } from "@mui/material"
+
+const Navbar=()=>{
+    return(
+        <AppBar>
+            <Toolbar>
+                <Typography>HEllo shahriar</Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+export default Navbar
